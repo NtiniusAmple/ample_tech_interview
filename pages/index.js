5adbe5c8128d4ba0967c7411b6f514fd63ddbe90
NtiniusAmple <68523298+NtiniusAmple@users.noreply.github.com>
2021-05-18 14:48:34 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import BrokenCode from "../components/BrokenCode";
 import styles from "../styles/Home.module.css";
+import {useState} from "react";
 
 // Fix all the broken code
 // Add a page for the chosen character
 // Use SSR for getting the character details.
 
 export default function Home() {
-	const name = prompt("Please enter your name", "Tai Chi Charles");
+	const [name, setName] = useState("TaiChi Charles");
 	return (
 		<div className={styles.container}>
 			<BrokenCode name={name} />
